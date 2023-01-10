e9450ad70ed035f47ca393136ccc0b031edfa26e
PatrickR89 <patrickr.89sc@gmail.com>
2023-01-10 22:11:12 +0100

    Add Structural Patterns

--- DesignPatterns/index.js
+++ DesignPatterns/index.js
@@ -1,19 +1,21 @@
-// console.log("Hello world!");
+// module pattern
+import express from "express";
+import calc from "./calc.js";
 
-// callback example
-// const calc = () => {
-//   return 4 * 4;
-// };
+const res = calc();
 
-// const printCalc = (callback) => {
-//   console.log(callback);
-// };
+const app = express();
+const port = 3000;
 
-// printCalc(calc());
+console.log(res);
 
-let instance = null;
+// Mixin pattern
 
-// class pattern
+let carMixin = {
+  revEngine() {
+    console.log("wroom!!!");
+  }
+};
 
 class Car {
   constructor(doors, engine, color) {
@@ -22,101 +24,26 @@ class Car {
     this.color = color;
   }
 }
+// add the mixin function to whatever instance is created from Car class
+Object.assign(Car.prototype, carMixin);
 
-const civic = new Car(4, "V6", "grey");
+let newCar = new Car(4, "I4", "black");
+newCar.revEngine();
 
-console.log(civic);
+// Facade pattern
+// code imported from another module and used in it's shorter version than original
 
-// constructor pattern
+// Flyweight pattern
+// similar to singleton
+// prevent creation of another instance
 
-class SUV extends Car {
-  constructor(doors, engine, color) {
-    super(doors, engine, color);
-    this.wheels = 4;
-  }
-}
+//Decorator pattern
+// very similar to mixins - typescript, add created function with @
 
-const cx5 = new SUV(4, "V8", "red");
-console.log(cx5);
+app.get("/", (req, res) => {
+  res.send(`server running on port ${port}`);
+});
 
-// singleton pattern
-
-class SecondCar {
-  constructor(engine, transmission, suspension, seats) {
-    if (!instance) {
-      this.engine = engine;
-      this.transmission = transmission;
-      this.suspension = suspension;
-      this.seats = seats;
-      instance = this;
-    } else {
-      return instance;
-    }
-  }
-}
-
-let miata = new SecondCar("I4", "5 speed", "sport", 4);
-let smart = new SecondCar("I3", "4spd auto", "bah", 2);
-
-console.log(miata);
-console.log(smart);
-
-// factory pattern
-
-class CarFactory {
-  createCar(type) {
-    switch (type) {
-      case "civic":
-        return new Car(4, "I4", "green");
-      case "cx5":
-        return new Car(4, "V8", "red/gray");
-      default:
-        break;
-    }
-  }
-}
-
-const factory = new CarFactory();
-
-const secondHonda = factory.createCar("civic");
-
-console.log(secondHonda);
-
-// abstract factory pattern
-
-class Crossover {
-  constructor(doors, engine, color) {
-    this.doors = doors;
-    this.engine = engine;
-    this.color = color;
-  }
-}
-
-class CrossoverFactory {
-  createCar(type) {
-    switch (type) {
-      case "santaFe":
-        return new Car(4, "I4", "green");
-      case "cx5":
-        return new Car(4, "V8", "red/gray");
-      default:
-        break;
-    }
-  }
-}
-const crossoverFactory = new CrossoverFactory();
-
-const autoManifacturer = (type, model) => {
-  switch (type) {
-    case "car":
-      return factory.createCar(model);
-    case "crossover":
-      return crossoverFactory.createCar(model);
-
-    default:
-      break;
-  }
-};
-
-const newCX5 = autoManifacturer("crossover", "cx5");
-console.log(newCX5);
+app.listen(port, () => {
+  console.log(`server on port ${port}`);
+});
